fcc1c3a35f70e09161e72275f0648901593ff537
zone <nokia_zou@163.com>
2016-06-19 22:20:32 +0800

    user

---
+++ SWPost/SWPost/Class/PostCell/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  SWPost
+//
+//  Created by 朋 邹 on 16/6/19.
+//  Copyright © 2016年 zou. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
